9867e5743a3bf118483940758d43469fbc0f3153
DaiPei <daipei@hust.edu.cn>
2017-04-13 11:06:47 +0800

    add comments for rowNumber

--- DPScrollNumberLabel/DPScrollNumberLabel.h
+++ DPScrollNumberLabel/DPScrollNumberLabel.h
@@ -18,9 +18,18 @@
 - (instancetype)initWithNumber:(NSNumber *)originNumber fontSize:(CGFloat)size textColor:(UIColor *)color;
 - (instancetype)initWithNumber:(NSNumber *)originNumber font:(UIFont *)font;
 - (instancetype)initWithNumber:(NSNumber *)originNumber font:(UIFont *)font textColor:(UIColor *)color;
+
+/********************************
+ 
+ if you set row number, the DPScrollNumberLabel instance's max num will less than the row num.
+ For example, you set row number to 1, then the max count of this label will be 9.
+ 
+ Note:rowNumber shouldn't more than 8
+ 
+ ********************************/
 - (instancetype)initWithNumber:(NSNumber *)originNumber fontSize:(CGFloat)size rowNumber:(NSUInteger)rowNumber;
 - (instancetype)initWithNumber:(NSNumber *)originNumber fontSize:(CGFloat)size textColor:(UIColor *)color rowNumber:(NSUInteger)rowNumber;
-- (instancetype)initWithNumber:(NSNumber *)originNumber font:(UIFont *)font textColor:(UIColor *)color rowNumber:(NSUInteger)rowNumber;//rowNumber should less than or equal 8
+- (instancetype)initWithNumber:(NSNumber *)originNumber font:(UIFont *)font textColor:(UIColor *)color rowNumber:(NSUInteger)rowNumber;
 
 - (void)changeToNumber:(NSNumber *)number animated:(BOOL)animated;
 - (void)changeToNumber:(NSNumber *)number interval:(CGFloat)interval animated:(BOOL)animated;
